4ea1b66dd773abf16a5d1c677bd971abc97a2cb6
Renato Figueiredo <renatofigueiredoalves@gmail.com>
2022-04-10 01:17:53 -0300

    test-passed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,31 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle != '') {
+      const Task = {
+        "id": Math.random(),
+        "title": newTaskTitle,
+        "isComplete": false,
+      };
+      setTasks(oldState => [...oldState, Task]);
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const Task = tasks.map(task => task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+    setTasks(Task);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const Task = tasks.filter(task => task.id != id);
+
+    setTasks(Task);
   }
 
   return (
